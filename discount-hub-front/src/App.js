05f432138cd7860f04a519acded2cd132cfc5956
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-09-16 17:33:35 -0400

    Add the dropdown component for the distance scroll

--- discount-hub-front/src/App.js
+++ discount-hub-front/src/App.js
@@ -1,19 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [zooms, setZoom] = useState(15)
 
   let zoom = (input) => {
     if (input > 1600) { return 2 }
-    else if (input > 800) {return 3 }
+    else if (input > 800) { return 3 }
     else if (input > 350) { return 4 }
-    else if (input > 160) {return 5 }
-    else if (input > 80) {return 6 }
-    else if (input > 40) {return 7 }
-    else if (input > 20) {return 8 }
-    else if (input > 10) {return 9 }
+    else if (input > 160) { return 5 }
+    else if (input > 80) { return 6 }
+    else if (input > 40) { return 7 }
+    else if (input > 20) { return 8 }
+    else if (input > 10) { return 9 }
     else if (input > 4) { return 10 }
-    else if (input > 2) { return 11}
+    else if (input > 2) { return 11 }
     else { return 15 }
   }
 
@@ -24,6 +26,34 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <form onSubmit={(e) => {e.preventDefault();
+          setZoom(zoom(document.getElementById('cars').value))}}>
+          <label>Select Distance:</label>
+          <select name="cars" id="cars">
+            <option value="1">1</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+            <option value="40">40</option>
+            <option value="50">50</option>
+            <option value="75">75</option>
+            <option value="100">100</option>
+            <option value="150">150</option>
+            <option value="200">200</option>
+            <option value="250">250</option>
+            <option value="500">500</option>
+            <option value="750">750</option>
+            <option value="1000">1000</option>
+            <option value="1500">1500</option>
+            <option value="2000">2000</option>
+            <option value="2500">2500</option>
+            <option value="3000">3000</option>
+          </select>
+          <input type="submit" value="Submit" ></input>
+        </form>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -41,7 +71,7 @@ function App() {
           allowFullScreen={true}
           referrerPolicy="no-referrer-when-downgrade"
           src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_SECRET_CODE}
-    &q=41.2565° N, 95.9345° W&zoom=3`}>
+    &q=41.2565° N, 95.9345° W&zoom=${zooms}`}>
         </iframe>
       </header>
     </div>
